bd2affbe977f79d96ea374f505898aa602f9ee35
KlangTjews <klangtjews@gmail.com>
2025-03-10 18:34:26 +0800

    项目重置密码功能实现

--- NovaChatGateServer/PostgreSQLMgr.cpp
+++ NovaChatGateServer/PostgreSQLMgr.cpp
@@ -23,10 +23,9 @@ int PostgreSQLMgr::RegUser(const std::string& name, const std::string& email, co
 		if (con == nullptr) {
 			return false;
 		}
+
 		pqxx::work tx{*(con->_con)};
-
 		pqxx::result res = tx.exec_params("SELECT reg_user($1, $2, $3)", name, pwd, email); // reg_user是返回整数的PostgreSQL函数
-
 		tx.commit();
 
 		// 处理结果
@@ -42,7 +41,7 @@ int PostgreSQLMgr::RegUser(const std::string& name, const std::string& email, co
 	}
 	catch (const pqxx::sql_error& e) {
 		pool_->returnConnection(std::move(con));
-		std::cerr << "SQLException: " << e.what();
+		std::cerr << "RegUser SQLException: " << e.what();
 		std::cerr << " (PostgreSQL SQLState: " << e.sqlstate() << ")" << std::endl;
 		return -1;
 	}
@@ -53,12 +52,64 @@ int PostgreSQLMgr::RegUserTransaction(const std::string& name, const std::string
 }
 
 bool PostgreSQLMgr::CheckEmail(const std::string& name, const std::string& email) {
-	return 1;
+	auto con = pool_->getConnection();
+	try {
+		if (con == nullptr) {
+			return false;
+		}
 
+		pqxx::work tx{ *(con->_con) };
+		std::string query = "SELECT user_email FROM \"user\" WHERE user_name = " + tx.quote(name);
+		pqxx::result res = tx.exec(query);
+		tx.commit();
+		pool_->returnConnection(std::move(con));
+
+		// 如果没有查询到记录，则返回 false
+		if (res.empty()) {
+			return false;
+		}
+
+		// 遍历结果集（通常应该只有一条记录）
+		for (const auto& row : res) {
+			std::string dbEmail = row["user_email"].as<std::string>();
+			std::cout << "Check Email: " << dbEmail << std::endl;
+			if (email != dbEmail) {
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
+	catch (const pqxx::sql_error& e) {
+		pool_->returnConnection(std::move(con));
+		std::cerr << "CheckEmail SQLException: " << e.what();
+		std::cerr << " (PostgreSQL SQLState: " << e.sqlstate() << ")" << std::endl;
+		return -1;
+	}
 }
 
 bool PostgreSQLMgr::UpdatePwd(const std::string& name, const std::string& newpwd) {
-	return 1;
+	auto con = pool_->getConnection();
+	try {
+		if (con == nullptr) {
+			return false;
+		}
+
+		pqxx::work tx{ *(con->_con) };
+		std::string query = "UPDATE \"user\" SET user_password = $1 WHERE user_name = $2";
+		pqxx::result res = tx.exec_params(query, newpwd, name);
+		tx.commit();
+		pool_->returnConnection(std::move(con));
+
+		std::cout << "Updated rows: " << res.affected_rows() << std::endl;
+		return true;
+	}
+	catch (const pqxx::sql_error& e) {
+		pool_->returnConnection(std::move(con));
+		std::cerr << "RegUser SQLException: " << e.what();
+		std::cerr << " (PostgreSQL SQLState: " << e.sqlstate() << ")" << std::endl;
+		return -1;
+	}
 }
 
 bool PostgreSQLMgr::CheckPwd(const std::string& name, const std::string& pwd, UserInfo& userInfo) {
